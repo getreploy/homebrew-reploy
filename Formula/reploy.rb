af2e8cd29bfbdef072fe6512cbe459a96e176c03
Jay Khatri <khatri.florida@gmail.com>
2020-03-28 21:40:30 -0400

    init

---
+++ Formula/reploy.rb
@@ -0,0 +1,10 @@
+class Reploy < Formula
+  desc "Manage your dev environments with ease :)"
+  homepage "https://github.com/getreploy/homebrew-reploy"
+  url "https://cli.getreploy.com/reploy.zip"
+  sha256 "cc293578bca43ba5cf0d60c1355c6aa1da9d923a0acc274a47ceab03812a6ef4"
+
+  def install
+    system "echo", "hi"
+  end
+end
