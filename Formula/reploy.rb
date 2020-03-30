d76b2b80b441c8ee099b91b463cfd552a5aef3a2
Jay Khatri <khatri.florida@gmail.com>
2020-03-30 17:16:52 -0400

    Change

--- Formula/reploy.rb
+++ Formula/reploy.rb
@@ -2,7 +2,7 @@ class Reploy < Formula
   desc "Manage your dev environments with ease :)"
   homepage "https://github.com/getreploy/homebrew-reploy"
   url "https://cli.getreploy.com/reploy.zip"
-  sha256 "cc293578bca43ba5cf0d60c1355c6aa1da9d923a0acc274a47ceab03812a6ef4"
+  sha256 %{echo -n reploy.rb | sha256sum}
 
   def install
     system "echo", "hi"
