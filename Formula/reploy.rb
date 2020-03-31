80e01caa682a18b0ac0a50ef7ad0c8d5877eb864
runner <jay-khatri@users.noreply.github.com>
2020-03-31 05:09:33 +0000

    Automated release commit

--- Formula/reploy.rb
+++ Formula/reploy.rb
@@ -1,11 +1,12 @@
 class Reploy < Formula
   desc "Manage your dev environments with ease :)"
   homepage "https://github.com/getreploy/homebrew-reploy"
+  version "0.0.1"
   url "https://cli.getreploy.com/reploy-latest.zip"
-  sha256 "72aa3dd579c3a770d819a065220fb004664166f6567c9b5c56a302d208f0d973"
+  sha256 "3bb6e00f8c22e4bc703f16aab5bf886f9d86fcc8965d8d50596040ae01bd42fe"
 
   def install
-    bin.install "doppler"
+    bin.install "reploy"
   end
 
   test do
