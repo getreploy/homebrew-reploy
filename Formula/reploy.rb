f698a12f8c8343b6ea9c2080cf8c31505b31ad23
runner <jay-khatri@users.noreply.github.com>
2020-03-31 18:54:38 +0000

    Automated release commit

--- Formula/reploy.rb
+++ Formula/reploy.rb
@@ -2,8 +2,8 @@ class Reploy < Formula
   desc "Manage your dev environments with ease :)"
   homepage "https://github.com/getreploy/homebrew-reploy"
   version "0.0.1"
-  url "https://cli.getreploy.com/reploy-44a1d2053d2eeed3feb958a59950205c79cf2e21.zip"
-  sha256 "c5dd207e68b10901feaa6b616f56545253aecc8903bd6d915a208439e82bd7fe"
+  url "https://cli.getreploy.com/reploy-c645ac729f750c183571be29ff97b0ac8791d7c9.zip"
+  sha256 "380275c2aed7f0ad50006d82cf2334a3270c7ebe98348e02d707f6476b015020"
 
   def install
     bin.install "reploy"
