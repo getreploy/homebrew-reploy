259d3c118d92b1e6d04b1c382f61dadb4208306a
runner <jay-khatri@users.noreply.github.com>
2020-03-31 20:33:08 +0000

    Automated release commit

--- Formula/reploy.rb
+++ Formula/reploy.rb
@@ -2,8 +2,8 @@ class Reploy < Formula
   desc "Manage your dev environments with ease :)"
   homepage "https://github.com/getreploy/homebrew-reploy"
   version "0.0.1"
-  url "https://cli.getreploy.com/reploy-8c01ae2bf8fc3923814725ff3c3cf3a7461971f4.zip"
-  sha256 "26bab19829c939d73126c73c3675f74c00ad844238488fa8b3b47feb768f2dcf"
+  url "https://cli.getreploy.com/reploy-506ad1402e3d7f009dd81fb661104e822de1b527.zip"
+  sha256 "0f414e29d02258b6a3587af187746b649883393eb091178c6d15490281272127"
 
   def install
     bin.install "reploy"
@@ -11,5 +11,37 @@ class Reploy < Formula
 
   test do
     system "test", "hi"
+  end
+
+  
+  plist_options :manual => "reploy watch"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+          </dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/reploy.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/reploy.log</string>
+        </dict>
+      </plist>
+    EOS
   end
 end
