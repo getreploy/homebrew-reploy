0b354516ea1e9e40d42a00d6a2eca5953303c634
runner <jay-khatri@users.noreply.github.com>
2020-03-31 06:21:47 +0000

    Automated release commit

--- Formula/reploy.rb
+++ Formula/reploy.rb
@@ -3,10 +3,10 @@ class Reploy < Formula
   homepage "https://github.com/getreploy/homebrew-reploy"
   version "0.0.1"
   url "https://cli.getreploy.com/reploy-latest.zip"
-  sha256 "de1ce529090131b87090f4471fc30ab88207ef2e82268e5f0fe53eaa5d6a062c"
+  sha256 "204095cd4f55050ba63e83cce3ed335b7e20c25a9dbd43caf65f9c3a10e8c77e"
 
   def install
-    bin.install "reploy-latest"
+    bin.install "reploy"
   end
 
   test do
